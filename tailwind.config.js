6893f972c8968d3e5d5366020be4a248e1ed2e90
aidongyang <aidongyang@vsulv.com>
2021-08-02 11:35:53 +0800

    build(env): 修改环境变量

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@
  * @Author: AiDongYang
  * @Date: 2021-07-29 17:57:17
  * @LastEditors: AiDongYang
- * @LastEditTime: 2021-08-02 11:19:00
+ * @LastEditTime: 2021-08-02 11:35:24
  */
 console.log(process.env.NODE_ENV)
 module.exports = {
