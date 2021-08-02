993292aa9914b4b78ce551b28227bffab498ccda
aidongyang <aidongyang@vsulv.com>
2021-08-02 11:42:33 +0800

    build(env): 修改环境变量

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,8 @@
  * @Author: AiDongYang
  * @Date: 2021-07-29 17:57:17
  * @LastEditors: AiDongYang
- * @LastEditTime: 2021-08-02 11:35:24
+ * @LastEditTime: 2021-08-02 11:42:28
  */
-console.log(process.env.NODE_ENV)
 module.exports = {
 	purge: {
 		enable: process.env.NODE_ENV === 'production',
